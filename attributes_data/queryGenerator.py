fcd1e34520376827c0ca2ab2736cb4be4ad7a2cd
Jordan Faas-Bush <jmanfb@gmail.com>
2018-12-08 20:49:26 -0500

    Added code to generate johnny depp SPARQL query and added more attributes

--- attributes_data/queryGenerator.py
+++ attributes_data/queryGenerator.py
@@ -1,7 +1,7 @@
 
 
 
-all_missclassified_intro_1 = """
+all_prefixes = """
 prefix fibo-fnd-arr-arr: <http://www.omg.org/spec/EDMC-FIBO/FND/Arrangements/Arrangements/>
 prefix lio: <http://purl.org/net/lio#>
 prefix lcc-lr: <http://www.omg.org/spec/LCC/Languages/LanguageRepresentation/>
@@ -17,34 +17,8 @@ prefix wt: <https://tw.rpi.edu/web/Courses/Ontologies/2018/FRMA/WearableThingsOn
 
 """
 
-all_results_text_1 = """
-select ?ResultSet ((?sideburnMiss/?totalMiss)*100 as ?sideburn_missclass_rate) ((?mustacheMiss/?totalMiss)*100 as ?mustache_missclass_rate)
-"""
-
 all_missclassified_intro_2 = """
 where {
-"""
-
-"""
-  ## calculates Mustache Missclassification
-  {
-    select ?ResultSet (count(?classification) as ?mustacheMiss)
-    where {
-      ?ResultSet a mlmo:ResultSet .
-      ?ResultSet fibo-fnd-arr-arr:hasConstituent ?Result .
-      ?Result lcc-lr:hasTag ?classification .
-      ?Result mlmo:hasFeature ?Image .
-
-      ?Image lio:depicts ?Person .
-      ?Person obo:BFO_0000051 ?facialHair .
-      ?facialHair a ho:Mustache .
-
-
-      ?Person fibo-fnd-aap-a:hasName ?Name .
-      filter (?classification != ?Name)
-    }
-    group by ?ResultSet
-  }
 """
 
 all_misclassified_generic_intro = """
@@ -59,6 +33,17 @@ all_misclassified_generic_intro = """
       ?Image lio:depicts ?Person .
 """
 
+johnny_depp_generic_intro = """
+  ## calculates how many instances of [lowercase_name] are associated with the person
+  {
+    select ?ResultSet (count(?classification) as ?[lowercase_name]Count)
+    where {
+      ?ResultSet a mlmo:ResultSet .
+      ?ResultSet fibo-fnd-arr-arr:hasConstituent ?Result .
+      ?Result mlmo:hasFeature ?Image .
+
+      ?Image lio:depicts ?Person .
+"""
 
 all_misclassified_generic_ending = """
       ?Person fibo-fnd-aap-a:hasName ?Name .
@@ -71,6 +56,30 @@ all_misclassified_generic_ending = """
   }
 """
 
+johnny_depp_generic_ending = """
+      optional{
+        ?Person fibo-fnd-aap-a:hasName "[person_name]"^^xsd:string .
+        filter (?classification = "[person_name]"^^xsd:string) # This line ensures we only have counts for correctly classified images
+        ?Result lcc-lr:hasTag ?classification .
+      }
+    }
+    group by ?ResultSet
+  }
+"""
+
+johnny_depp_generic_model_tagged_ending = """
+      optional{
+        ?Result lcc-lr:hasTag "[person_name]"^^xsd:string .
+        ?Result lcc-lr:hasTag ?classification .
+      }
+    }
+    group by ?ResultSet
+  }
+"""
+
+johnny_depp_ending = """
+}
+"""
 
 all_misclassified_ending = """
   # Calulates the total number of incorrect Images
@@ -99,10 +108,7 @@ all_misclassified_ending = """
 def generate_misclassified_attributes(attributes):
 	# attributes is a list of tuples:
 	#[  (lowercase_name, [hierarchy way to get to it from Person])  ]
-	output_text = all_missclassified_intro_1
-
-
-
+	output_text = all_prefixes
 
 	# then generate all the results that we're finding for the header of the query
 	#select ?ResultSet ((?sideburnMiss/?totalMiss)*100 as ?sideburn_missclass_rate) ((?mustacheMiss/?totalMiss)*100 as ?mustache_missclass_rate)
@@ -130,6 +136,39 @@ def generate_misclassified_attributes(attributes):
 	output_text += all_misclassified_ending
 	return output_text
 
+def generate_johnny_depp(attributes, person_name, use_all_images_tagged = False):
+	# attributes is a list of tuples:
+	#[  (lowercase_name, [hierarchy way to get to it from Person])  ]
+	output_text = all_prefixes
+
+	# then generate all the results that we're finding for the header of the query
+	#select ?ResultSet ((?sideburnMiss/?totalMiss)*100 as ?sideburn_missclass_rate) ((?mustacheMiss/?totalMiss)*100 as ?mustache_missclass_rate)
+	all_results_list = []
+	for a in attributes:
+		feature = a[0].lower()
+		t = "?" + str(feature) + "Count"
+		all_results_list.append(t)
+	all_results = "select ?ResultSet " + " ".join(all_results_list)
+	output_text += all_results
+
+	# then add the second intro text
+	output_text += all_missclassified_intro_2
+
+	# then loop over all the attributes adding the attributes in
+	for a in attributes:
+		feature = a[0].lower()
+		a_text = johnny_depp_generic_intro.replace("[lowercase_name]", feature)
+		for hierarchy in a[1]:
+			# loop through the way to get to the attribute adding it to the text:
+			a_text += "      " + hierarchy + "\n"
+		if use_all_images_tagged:
+			a_text += johnny_depp_generic_model_tagged_ending.replace("[person_name]", person_name)
+		else:
+			a_text += johnny_depp_generic_ending.replace("[person_name]", person_name)
+		output_text += a_text
+
+	output_text += johnny_depp_ending
+	return output_text
 
 if __name__ == "__main__":
 	
@@ -138,6 +177,8 @@ if __name__ == "__main__":
 	all_attributes = [
 		("sideburn", ["?Person obo:BFO_0000051 ?facialHair .", "?facialHair a ho:Sideburn ."]),
 		("mustache", ["?Person obo:BFO_0000051 ?facialHair .", "?facialHair a ho:Mustache ."]),
+		("FiveOClockShadow", ["?Person obo:BFO_0000051 ?facialHair .", "?facialHair a ho:FiveOClockShadow ."]),
+		("Goatee", ["?Person obo:BFO_0000051 ?facialHair .", "?facialHair a ho:Goatee ."]),
 		
 		("White", ["?Person pfd:hasDemographic ?ethnicity .", "?ethnicity a pfd:White ."]),
 		("Asian", ["?Person pfd:hasDemographic ?ethnicity .", "?ethnicity a pfd:Asian ."]),
@@ -158,6 +199,24 @@ if __name__ == "__main__":
 
 		("Chubby", ["?Person pfd:hasDemographic ?weightRange .", "?weightRange a pfd:Chubby ."]),
 		("Skinny", ["?Person pfd:hasDemographic ?weightRange .", "?weightRange a pfd:Skinny ."]),
+		("BrownEye", ["?Person obo:BFO_0000051 ?face .", "?face obo:BFO_0000051 ?eye .", "?eye a obo:UBERON_0000970 .", "?eye pfd:hasColor ?eyeColor .", "?eyeColor a pfd:Brown."]),
+		("Eyeglasses", ["?Person wt:isWearing ?wearable .", "?wearable a wt:Eyeglasses ."]),
+		("Sunglasses", ["?Person wt:isWearing ?wearable .", "?wearable a wt:Sunglasses ."]),
+		("Earrings", ["?Person wt:isWearing ?wearable .", "?wearable a wt:Earrings ."]),
+		("Hat", ["?Person wt:isWearing ?wearable .", "?wearable a wt:Hat ."]),
+		("Lipstick", ["?Person wt:isWearing ?wearable .", "?wearable a wt:Lipstick ."]),
+		("NeckTie", ["?Person wt:isWearing ?wearable .", "?wearable a wt:NeckTie ."]),
+		("Necklace", ["?Person wt:isWearing ?wearable .", "?wearable a wt:Necklace ."]),
+		("Bald", ["?Person obo:BFO_0000051 ?headhair .", "?headhair a ho:HeadHair .", "?headhair ho:hasHaircut ?haircut .", "?haircut a ho:Bald ."]),
+		("Bangs", ["?Person obo:BFO_0000051 ?headhair .", "?headhair a ho:HeadHair .", "?headhair ho:hasHaircut ?haircut .", "?haircut a ho:Bangs ."]),
+		
+		("BalancedLightingVariation", ["?Image img:lightingDescribedBy ?lightingDescription .", "?lightingDescription a img:BalancedLightingVariation ."]),
+		("HarshLightingVariation", ["?Image img:lightingDescribedBy ?lightingDescription .", "?lightingDescription a img:HarshLightingVariation ."]),
+		("SoftLightingVariation", ["?Image img:lightingDescribedBy ?lightingDescription .", "?lightingDescription a img:SoftLightingVariation ."]),
+
+		("BlurryImageFidelity", ["?Image img:fidelityDescribedBy ?imageFidelity .", "?imageFidelity a img:BlurryImageFidelity ."]),
+		("SharpImageFidelity", ["?Image img:fidelityDescribedBy ?imageFidelity .", "?imageFidelity a img:SharpImageFidelity ."]),
+
 		]
 
 	# uncomment this to generate the query for all misclassified attributes:
@@ -165,4 +224,4 @@ if __name__ == "__main__":
 
 
 	# uncomment this to generate the query for Johnny Depp:
-	
+	# print(generate_johnny_depp(all_attributes, "George W Bush", True))
